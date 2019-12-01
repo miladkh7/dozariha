78c9172ca5e3f3221175e627099f8cfada51da47
milad <milad_khaleghi@live.com>
2019-12-01 13:43:41 +0330

    add index view-route urls-add css style

--- webVersion/urls.py
+++ webVersion/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-
+from django.urls import path
+from . import views
+from django.conf.urls import url
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('webVersion.urls'))
-
+    # path('admin/', admin.site.urls),
+    url(r'',views.index)
 ]
--- webVersion/views.py
+++ webVersion/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Comment,Influencer
 # Create your views here.
+def index(request):
+    Influencers=Influencer.objects.all()
+    comments=Comment.objects.filter(state='approve')
+    content={}
+    content['comments']=comments
+    # content={'influnecers':Influencers}
+    return render(request,'index.html',content)
