94a4055bda595d144700ffa33632f79692b591c7
milad <milad_khaleghi@live.com>
2019-12-04 01:44:07 +0330

    add AutoComplete feature to influncer profile

--- webVersion/urls.py
+++ webVersion/urls.py
@@ -23,7 +23,8 @@ from django.conf import settings
 urlpatterns = [
     # path('admin/', admin.site.urls),
     url(r'^home',views.index),
-    url(r'^submit_comment/',views.submit_comment,name='submit')
+    url(r'^submit_comment/',views.submit_comment,name='submit'),
+    url(r'^ajax_calls/search/',views.autocompleteModel,name='search'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- webVersion/views.py
+++ webVersion/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,get_object_or_404
 from .models import Comment,Influencer
 from django.http import HttpResponse,HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
+import json
+
 # Create your views here.
 def index(request):
     # Influencers=Influencer.objects.all()
@@ -21,6 +23,23 @@ def submit_comment(request):
     # print('success')
     # this_influncer=Influencer.objects.filter(profile_name=infulncerID).get()
     this_influncer=get_object_or_404(Influencer, profile_name=infulncerID)
-    if this_influncer:
-        Comment.objects.create(Influencer=this_influncer,comment_text=comment,state="approve")
-        return HttpResponseRedirect('/home/')
+    # if this_influncer:
+    Comment.objects.create(Influencer=this_influncer,comment_text=comment,state="approve")
+    return HttpResponseRedirect('/home/')
+
+def autocompleteModel(request):
+    if request.is_ajax():
+        print('salam')
+        q = request.GET.get('term', '').capitalize()
+        print(q)
+        # qq=get_object_or_404(Influencer, profile_name__startswith=q))
+        search_qs = Influencer.objects.filter(profile_name__startswith=q)
+        results = []
+
+        for r in search_qs:
+            results.append(r.profile_name)
+        data = json.dumps(results)
+    else:
+        data = 'fail'
+    mimetype = 'application/json'
+    return HttpResponse(data, mimetype)
